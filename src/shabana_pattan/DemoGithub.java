36ec749012adbb51a6c2ac272722dfb949c99770
SDDRP <155455876+SDDRP@users.noreply.github.com>
2024-01-03 19:15:52 +0530

    Demo Github Class Added

---
+++ src/shabana_pattan/DemoGithub.java
@@ -0,0 +1,14 @@
+package shabana_pattan;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class DemoGithub {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}

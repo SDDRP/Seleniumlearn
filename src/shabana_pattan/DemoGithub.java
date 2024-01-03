748c83fd0634e1f07c75c8b6dd7912ebd0071547
SDDRP <155455876+SDDRP@users.noreply.github.com>
2024-01-03 19:21:27 +0530

    Updated code

--- src/shabana_pattan/DemoGithub.java
+++ src/shabana_pattan/DemoGithub.java
@@ -1,14 +1,22 @@
 package shabana_pattan;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class DemoGithub {
+	WebDriver driver;
+	
   @Test
-  public void f() {
+  public void google() {
   }
+  
+  
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }

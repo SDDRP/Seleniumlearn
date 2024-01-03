47f898bc51bdf0846e21b8e32262ef42e90182af
SDDRP <155455876+SDDRP@users.noreply.github.com>
2024-01-03 19:29:13 +0530

    Push selenium code

---
+++ src/shaheena_pattan/SeleniumGithub.java
@@ -0,0 +1,14 @@
+package shaheena_pattan;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class SeleniumGithub {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
